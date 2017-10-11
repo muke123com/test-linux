2819c5512100a502b202f8d323790d1cd0aef5c4
mukeke <mukeke@quandashi.com>
2017-10-11 17:22:28 +0800

    aa

--- routes/process.js
+++ routes/process.js
@@ -1,9 +1,11 @@
 var express = require('express');
 var router = express.Router();
 
-/* GET users listing. */
 router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+	console.log(process);
+	res.send({
+		"aaa": "111"
+	});
 });
 
 module.exports = router;
