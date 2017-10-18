7bce740cad52063750833822afb02362c67bc557
mukeke <mukeke@quandashi.com>
2017-10-18 18:35:23 +0800

    fortune

--- routes/process.js
+++ routes/process.js
@@ -1,10 +1,23 @@
 var express = require('express');
 var os = require('os');
+var spawn = require('child_process').spawn;
+var exec = require('child_process').exec; 
+var cmdStr = 'fortune';
+
 var router = express.Router();
 
 router.get('/', function(req, res, next) {
-	console.log();
-	res.end();
+	exec(cmdStr, function(err,stdout,stderr){
+	    if(err) {
+	        console.log('error:'+stderr);
+	        res.end();
+	    } else {
+	        console.log(stdout);
+	        res.send({
+	        	msg: stdout
+	        });
+	    }
+	});
 });
 
 router.get('/getip', function(req, res, next) {
