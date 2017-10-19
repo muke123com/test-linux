dd108defad84b86db10c664ee01a9974d53f39a3
mukeke <mukeke@quandashi.com>
2017-10-19 14:47:48 +0800

    shell

--- routes/process.js
+++ routes/process.js
@@ -10,10 +10,32 @@ router.get('/', function(req, res, next) {
 	exec(cmdStr, function(err,stdout,stderr){
 	    if(err) {
 	        console.log('error:'+stderr);
-	        res.end();
+	        res.render("index", {
+	        	msg: "When nothing seems to help,I go and look at a stonecutter hammering away at\
+					his rock perhaps a hundred times without as much as a crack showing in\
+					it.Yet at the hundred and first blowit will split in two,and I know it was\
+					not that blow that did it —— but all that had gone before.  ——Jacob Riis"
+	        })
 	    } else {
 	    	stdout.split("\n").join("<br />").split("\t").join("&nbsp;&nbsp;");
+	    	console.log(stdout);
 	        res.render("index", {
+	        	msg: stdout
+	        })
+	    }
+	});
+});
+
+router.post('/shell', function(req, res, next) {
+	var shell = req.body.shell;
+	exec(shell, function(err,stdout,stderr){
+	    if(err) {
+	        console.log('error:'+stderr);
+	        res.send({
+	        	error: stderr
+	        })
+	    } else {
+	        res.send({
 	        	msg: stdout
 	        })
 	    }
